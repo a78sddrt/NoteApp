b2d1837c98075959478361e122ed46c4567d4812
a78sddrt <88551093+a78sddrt@users.noreply.github.com>
2024-08-09 13:24:13 +0800

    Initial Commit

---
+++ Note/NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by 王崇亘 on 2024/8/9.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
